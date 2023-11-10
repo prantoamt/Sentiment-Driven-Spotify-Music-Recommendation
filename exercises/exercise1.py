ce68df95394c2ad556cd2f52b1ee6c3bed8082fd
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2023-11-10 09:25:58 +0100

    transform_data fuction added

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -31,8 +31,14 @@ class DataPipeline:
         except Exception as e:
             sys.exit(1)
         return file_path
+    
+    def transform_data(self, file_path: str) -> pd.DataFrame:
+        data_frame = pd.read_csv(
+            file_path, sep=";", header=0, names=None, compression=None, encoding="utf-8"
+        )
+        return data_frame
 
-    def load_data(self, data_frame: pd.DataFrame):
+    def load_data(self, data_frame: pd.DataFrame) -> None:
         try:
             # connect to the database
             connection = sqlite3.connect(self.output_db.db_name)
@@ -49,9 +55,7 @@ class DataPipeline:
 
     def run_pipeline(self) -> None:
         file_path = self.extract_data()
-        data_frame = pd.read_csv(
-            file_path, sep=";", header=0, names=None, compression=None, encoding="utf-8"
-        )
+        data_frame = self.transform_data(file_path=file_path)
         self.load_data(data_frame=data_frame)
 
 
