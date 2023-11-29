1f7429addf090880355a475a8a841ad482e65cce
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2023-11-29 18:09:43 +0100

    New services adapted

--- project/pipeline_entrypoint.py
+++ project/pipeline_entrypoint.py
@@ -8,19 +8,19 @@ import pandas as pd
 # Self imports6
 from services.pipeline import (
     ETLPipeline,
-    DataSource,
-    CSVFile,
-    SQLiteDB,
+    DataExtractor,
+    CSVInterpreter,
+    SQLiteLoader,
     ETLQueue,
 )
 
 DATA_DIRECTORY = os.path.join(os.getcwd(), "data")
 
 def construct_songs_pipeline() -> ETLPipeline:
-    songs_output_db = SQLiteDB(
+    songs_output_db = SQLiteLoader(
         db_name="project.sqlite",
         table_name="song_lyrics",
-        if_exists=SQLiteDB.REPLACE,
+        if_exists=SQLiteLoader.REPLACE,
         index=False,
         method=None,
         output_directory=DATA_DIRECTORY,
@@ -38,31 +38,31 @@ def construct_songs_pipeline() -> ETLPipeline:
         data_frame = data_frame.rename(columns={"seq": "lyrics"})
         return data_frame
 
-    songs_file = CSVFile(
+    songs_interpreter = CSVInterpreter(
         file_name="labeled_lyrics_cleaned.csv",
         sep=",",
         names=None,
         dtype=songs_file_dtype,
         transform=transform_lyrics,
     )
-    songs_data_source = DataSource(
+    songs_data_source = DataExtractor(
         data_name="Song lyrics",
         url="https://www.kaggle.com/datasets/edenbd/150k-lyrics-labeled-with-spotify-valence",
-        source_type=DataSource.KAGGLE_DATA,
-        files=(songs_file,),
+        type=DataExtractor.KAGGLE_ARCHIVE,
+        interpreters=(songs_interpreter,),
     )
     songs_pipeline = ETLPipeline(
-        data_source=songs_data_source,
-        sqlite_db=songs_output_db,
+        data_extractor=songs_data_source,
+        loader=songs_output_db,
     )
     return songs_pipeline
 
 
 def construct_twitter_pipeline() -> ETLPipeline:
-    twitter_output_db = SQLiteDB(
+    twitter_output_db = SQLiteLoader(
         db_name="project.sqlite",
         table_name="tweets",
-        if_exists=SQLiteDB.REPLACE,
+        if_exists=SQLiteLoader.REPLACE,
         index=False,
         method=None,
         output_directory=DATA_DIRECTORY,
@@ -71,21 +71,21 @@ def construct_twitter_pipeline() -> ETLPipeline:
         "clean_text": str,
         "category": "Int64",
     }
-    twitter_file = CSVFile(
+    twitter_interpreter = CSVInterpreter(
         file_name="Twitter_Data.csv",
         sep=",",
         names=None,
         dtype=twitter_file_dtype,
     )
-    twitter_data_source = DataSource(
+    twitter_data_source = DataExtractor(
         data_name="Twitter",
         url="https://www.kaggle.com/datasets/saurabhshahane/twitter-sentiment-dataset/",
-        source_type=DataSource.KAGGLE_DATA,
-        files=(twitter_file,),
+        type=DataExtractor.KAGGLE_ARCHIVE,
+        interpreters=(twitter_interpreter,),
     )
     twitter_pipeline = ETLPipeline(
-        data_source=twitter_data_source,
-        sqlite_db=twitter_output_db,
+        data_extractor=twitter_data_source,
+        loader=twitter_output_db,
     )
     return twitter_pipeline
 
