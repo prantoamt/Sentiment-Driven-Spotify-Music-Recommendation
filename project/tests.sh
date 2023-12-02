520370adbbe4488bfd1e4d5ef5565bd1350f1a14
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2023-12-02 12:39:48 +0100

    tests.sh created

---
+++ project/tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pytest project/tests/tests_system.py
+
+
+# To run the tests you need kaggle credentials to pull data from kaggle.
+# To do so: 
+#   1. please download the kaggle.json file from the link (https://docs.google.com/document/d/1nTbYoqtzhuq4R3yBovNVnRnj6mMbF53qxltKSBYNfPE/edit?usp=sharing)
+#   2. put kaggle.json in root directory.
+#   3. run chmod +x ./project/tests.sh
+#   4. run ./project/tests.sh
--- project/tests/tests_system.py
+++ project/tests/tests_system.py
@@ -1,17 +1,13 @@
 # Python imports
-import os
-import sqlite3
+import os, sys
+import logging
 # Third-party imports
-import numpy as np
+import sqlite3
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
 # Self imports
 from etl_pipeline_runner.services import (
-    ETLPipeline,
-    DataExtractor,
-    CSVHandler,
-    SQLiteLoader,
     ETLQueue,
 )
 
@@ -20,9 +16,14 @@ class TestPipeline:
         loader = songs_pipeline.extractor.file_handlers[0].loader
         ETLQueue(etl_pipelines=(songs_pipeline,)).run()
         db_path = os.path.join(loader.output_directory, loader.db_name)
-        connection = sqlite3.connect(db_path)
-        result = pd.read_sql_query(f"SELECT * FROM {loader.table_name}", connection)
-        connection.close()
+        try:
+            connection = sqlite3.connect(db_path)
+            result = pd.read_sql_query(f"SELECT * FROM {loader.table_name}", connection)
+        except sqlite3.Error as e:
+            logging.error(msg=f"Error while creating SQLite DB: {e}")
+            sys.exit(1)
+        finally:
+            connection.close()
         transformed_data = songs_pipeline.extractor.file_handlers[0]._data_frame
         assert_frame_equal(result, transformed_data)
         os.remove(db_path)
@@ -31,9 +32,14 @@ class TestPipeline:
         loader = twitter_pipeline.extractor.file_handlers[0].loader
         ETLQueue(etl_pipelines=(twitter_pipeline,)).run()
         db_path = os.path.join(loader.output_directory, loader.db_name)
-        connection = sqlite3.connect(db_path)
-        result = pd.read_sql_query(f"SELECT * FROM {loader.table_name}", connection)
-        connection.close()
+        try:
+            connection = sqlite3.connect(db_path)
+            result = pd.read_sql_query(f"SELECT * FROM {loader.table_name}", connection)
+        except sqlite3.Error as e:
+            logging.error(msg=f"Error while creating SQLite DB: {e}")
+            sys.exit(1)
+        finally:
+            connection.close()
         transformed_data = twitter_pipeline.extractor.file_handlers[0]._data_frame
         assert_frame_equal(result, transformed_data)
         os.remove(db_path)
